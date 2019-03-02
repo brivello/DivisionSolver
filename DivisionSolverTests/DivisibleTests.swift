c25fa103fef2466a1561d0b83b65197463615531
Ben Rivello <[tub62574@temple.edu]>
2019-03-02 17:55:06 -0500

    wrote test case for size 1 arrays

--- DivisionSolverTests/DivisibleTests.swift
+++ DivisionSolverTests/DivisibleTests.swift
@@ -11,9 +11,14 @@ import XCTest
 @testable import DivisionSolver
 
 class DivisibleTests: XCTestCase {
+
+    let randomMin            = -1000000.0
+    let randomMax            =  1000000.0
+    let randomTestLength     =  100
     
     func testIsUsefull() {
         let problem: [Any] = [16.0, [20.0, 4.0], 22.0]
+        XCTAssertTrue((16.0 / (20.0 / 4.0) / 22.0) != (16.0 / 20.0 / 4.0 / 22.0))
         XCTAssertTrue(problem.divide() == 16.0 / (20.0 / 4.0) / 22.0)
         XCTAssertTrue(problem.divide() != 16.0 / 20.0 / 4.0 / 22.0)
     }
@@ -21,48 +26,73 @@ class DivisibleTests: XCTestCase {
     func testDivideZero () {
         let problem = [4.0, 0.0]
         XCTAssertTrue(problem.divide() == (4.0 / 0.0))
+        XCTAssertTrue(problem.divide().isInfinite)
+    }
+    
+    func testInvalidArray () {
+        let problem:[Any] = [19.0, ["String", Data.init(), -4 ], true]
+        XCTAssertTrue(problem.divide().isNaN)
+    }
+    
+    func testExtraArrays () {
+        let problem = [[[4.0, 2.3]]]
+        XCTAssertTrue(problem.divide() == (4.0 / 2.3))
+    }
+    
+    func testSampleProblem() {
+        XCTAssertTrue([[16.0, [8.0,2.0],4.0],2.0,80.0].divide() ==   ((16 / (8 / 2) / 4) / 2 / 80)   )
     }
     
     func testDivideDoubles () {
-        for _ in 0...1000 {
-            let dbl1 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl2 = Double.random(in: -1000000.0..<1000001.0)
+        for _ in 0...randomTestLength {
+            let dbl1 = Double.random(in: randomMin..<randomMax)
+            let dbl2 = Double.random(in: randomMin..<randomMax)
             XCTAssertTrue([dbl1, dbl2].divide() == (dbl1 / dbl2))
         }
     }
     
     func testDivideArrays() {
-        for _ in 0...1000 {
-            let dbl1 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl2 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl3 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl4 = Double.random(in: -1000000.0..<1000001.0)
+        for _ in 0...randomTestLength {
+            let dbl1 = Double.random(in: randomMin..<randomMax)
+            let dbl2 = Double.random(in: randomMin..<randomMax)
+            let dbl3 = Double.random(in: randomMin..<randomMax)
+            let dbl4 = Double.random(in: randomMin..<randomMax)
             let solution = [[dbl1, dbl2], [dbl3, dbl4]].divide()
             XCTAssertTrue(solution == (dbl1 / dbl2) / (dbl3 / dbl4))
         }
     }
     
     func testDivideUnbalancedArrays() {
-        for _ in 0...1000 {
-            let dbl1 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl2 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl3 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl4 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl5 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl6 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl7 = Double.random(in: -1000000.0..<1000001.0)
-            let dbl8 = Double.random(in: -1000000.0..<1000001.0)
+        for _ in 0...randomTestLength {
+            let dbl1 = Double.random(in: randomMin..<randomMax)
+            let dbl2 = Double.random(in: randomMin..<randomMax)
+            let dbl3 = Double.random(in: randomMin..<randomMax)
+            let dbl4 = Double.random(in: randomMin..<randomMax)
+            let dbl5 = Double.random(in: randomMin..<randomMax)
+            let dbl6 = Double.random(in: randomMin..<randomMax)
+            let dbl7 = Double.random(in: randomMin..<randomMax)
+            let dbl8 = Double.random(in: randomMin..<randomMax)
             let solution = [dbl1, [dbl2, dbl3], dbl4, [dbl5, dbl6, dbl7], dbl8].divide()
             XCTAssertTrue(solution == dbl1 / (dbl2 / dbl3) / dbl4 / (dbl5 / dbl6 / dbl7) / dbl8)
         }
     }
     
-    func testSampleProblem() {
-        XCTAssertTrue([[16.0, [8.0,2.0],4.0],2.0,80.0].divide() == (16 / (8 / 2) / 4) / 2 / 80)
-    }
-    
-    func testInvalidArray () {
-        let problem:[Any] = [19.0, ["String", Data.init(), -4 ], true]
-        XCTAssertTrue(problem.divide().isNaN)
+    func testDivideNestedUnbalancedArrays() {
+        for _ in 0...randomTestLength {
+            let dbl1  = Double.random(in: randomMin..<randomMax)
+            let dbl2  = Double.random(in: randomMin..<randomMax)
+            let dbl3  = Double.random(in: randomMin..<randomMax)
+            let dbl4  = Double.random(in: randomMin..<randomMax)
+            let dbl5  = Double.random(in: randomMin..<randomMax)
+            let dbl6  = Double.random(in: randomMin..<randomMax)
+            let dbl7  = Double.random(in: randomMin..<randomMax)
+            let dbl8  = Double.random(in: randomMin..<randomMax)
+            let dbl9  = Double.random(in: randomMin..<randomMax)
+            let dbl10 = Double.random(in: randomMin..<randomMax)
+            let dbl11 = Double.random(in: randomMin..<randomMax)
+            let dbl12 = Double.random(in: randomMin..<randomMax)
+            let solution = [dbl1, [dbl2, [dbl3, dbl4], [dbl5, [dbl6, dbl7], dbl8, dbl9], dbl10, dbl11], dbl12].divide()
+            XCTAssertTrue(solution == dbl1 / (dbl2 / (dbl3 / dbl4) / (dbl5 / (dbl6 / dbl7) / dbl8 / dbl9) / dbl10 / dbl11) / dbl12)
+        }
     }
 }
