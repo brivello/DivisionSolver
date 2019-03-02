1110625aef48f0d2894938088c5d29cab6c80d77
Ben Rivello <[tub62574@temple.edu]>
2019-03-02 17:24:32 -0500

    added Framework to repository

---
+++ Framework/DivisionSolver.framework/Versions/A/Headers/DivisionSolver.h
@@ -0,0 +1,19 @@
+//
+//  DivisionSolver.h
+//  DivisionSolver
+//
+//  Created by Developer on 3/2/19.
+//  
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DivisionSolver.
+FOUNDATION_EXPORT double DivisionSolverVersionNumber;
+
+//! Project version string for DivisionSolver.
+FOUNDATION_EXPORT const unsigned char DivisionSolverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DivisionSolver/PublicHeader.h>
+
+
